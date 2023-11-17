915753aaf4dd964cacd22d2701b95d19ba14f804
Sergii Nagornyi <snagornyi@deloitte.com>
2023-11-18 00:27:49 +0200

    run react app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from 'react-dom/client';
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDom.render(
+
+require("./styles.css")
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,43 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
+    filename: "[name]-boundle.js",
     path: path.resolve(__dirname, "public"),
-    clean: true,
+    publicPath: "/",
   },
+  mode: "development",
   devtool: "source-map",
   devServer: {
     hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{ loader: "babel-loader" }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html",
+            },
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
   },
 };
